79fcf632afb9b5a6410c41b10868f60b4523bec9
Guilherme Beyruti Suranyi <guilhermesuranyi@DN0a1fae5f.SUNet>
2023-05-17 17:17:21 -0700

    add examples and makefile

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from calculator_adapter import run
 
-
-### ADD AT LEAST TWO TESTS HERE!
-
+assert run("5/5").output == "1"
+assert run("5/5").exit_status == 0
+###
+assert run("5!1").exit_status != 0
+assert run("5-4").output == "1"
 
 ###
 
